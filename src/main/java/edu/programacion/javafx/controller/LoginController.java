38044584604cb9605057f4aabcbce208c2d5e476
ProfeFS <profefs@outlook.com>
2024-05-24 14:51:57 +0200

    no message

--- src/main/java/edu/programacion/javafx/controller/LoginController.java
+++ src/main/java/edu/programacion/javafx/controller/LoginController.java
@@ -1,16 +1,25 @@
 package edu.programacion.javafx.controller;
 
-import java.nio.file.spi.FileSystemProvider;
-import java.util.List;
+import java.io.IOException;
 
 import edu.programacion.javafx.model.User;
 import edu.programacion.javafx.service.UserService;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
+	private static final String PATH = "/edu/programacion/javafx/vista/";
+
 	@FXML
 	private TextField txtLogUser;
 
@@ -27,31 +36,56 @@ public class LoginController {
 	private UserService userService;
 	private String logedUserRol;
 
+	private UsersController usersController;
+
 	public LoginController() {
 		userService = new UserService();
 	}
 
-	public void login() {
+	public void login(ActionEvent event) {
 		user = userService.getUserByLoginName(txtLogUser.getText());
 		if (user != null) {
-			if(isValidPassword()) {
-				showUserView();
-			}else {
+			if (isValidPassword()) {
+				showUserView(event);
+			} else {
 				showAlert("Pasword inválido");
 			}
-		}else {
+		} else {
 			showAlert("Usuario inválido");
 		}
 	}
 
 	private void showAlert(String string) {
-		// TODO Auto-generated method stub
-		
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Error de Inicio de Sesión");
+		alert.setHeaderText(null);
+		alert.setContentText(string);
+		alert.showAndWait();
 	}
 
-	private void showUserView() {
-		// TODO Auto-generated method stub
-		
+	private void showUserView(ActionEvent event) {
+		// Carga el nuevo contenido FXML
+					FXMLLoader loader = new FXMLLoader(getClass().getResource("Users.fxml"));
+					Parent root;
+					
+					try {
+						root = loader.load();
+						usersController = loader.getController();
+						usersController.setLogedUserRol(logedUserRol);
+						// Obtiene el Stage actual a partir del evento del botón
+						Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+						// Crea una nueva Scene con el nuevo root
+						Scene scene = new Scene(root);
+
+						// Establece la nueva Scene en el Stage
+						stage.setScene(scene);
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}				
+
 	}
 
 	private boolean isValidPassword() {
